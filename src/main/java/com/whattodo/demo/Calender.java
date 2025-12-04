87f058c20608c194411e6f29d6eaf17b35199fa4
BilalHamaydi <bhamaydi@gmail.com>
2025-12-04 14:04:21 +0100

    application properties done

--- src/main/java/com/whattodo/demo/Calender.java
+++ src/main/java/com/whattodo/demo/Calender.java
@@ -1,7 +1,16 @@
 package com.whattodo.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
 public class Calender {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     String dayName;
     String dayNumber;
     boolean isHoliday;
@@ -9,6 +18,7 @@ public class Calender {
     String year;
     int week;
 
+    public Calender(){}
     public Calender(int week){
     //this.week
     }
